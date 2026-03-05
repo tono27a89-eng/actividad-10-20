8a1bf63a06578dfc4e67aad88c99342fd8fb1328
gerberth Antonio Englenton de León <tono27a89@gmail.com>
2026-03-04 23:28:49 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_10_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cree un diccionario donde la clave sea el carné (int) y el valor sea una tupla con Nombre, Edad y Carrera. Permita agregar 3 estudiantes y luego muestre toda la información usando foreach.");
+        }
+    }
+}
